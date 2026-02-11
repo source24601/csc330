c1b85a13686467e15e63053961240dbe025a0acc
source24601 <morganlewisldl@gmail.com>
2026-02-11 03:46:42 +0000

    orp

--- orphan.py
+++ orphan.py
@@ -1,3 +1,14 @@
-#test to see if the file just doesn't show up
+import os
+import time
 
-#yeah
+pid = os.fork()
+
+if pid == 0:  # Child process
+    print(f"Child ({os.getpid()}): Working for 30 seconds...")
+    time.sleep(30)
+    print(f"Child ({os.getpid()}) finished")
+else:  # Parent process
+    print(f"Parent ({os.getpid()}) started")
+    
+    print(f"Parent ({os.getpid()}) finished")
+
--- zombie.py
+++ zombie.py
@@ -1,5 +1,14 @@
-#Also testing zombie
+import os
+import time
 
+pid = os.fork()
 
+if pid == 0:  # Child process
+    print(f"Child ({os.getpid()}): Working for 2 seconds...")
 
-#yeah
+    print(f"Child ({os.getpid()}) finished")
+else:  # Parent process
+    print(f"Parent ({os.getpid()}) started")
+    time.sleep(30)
+    print(f"Parent ({os.getpid()}) finished")
+
